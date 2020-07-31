c7e8c72a45ded1bb620bc01547b9aac6b389a726
RodrigoDornelles <rodrigo@dornelles.me>
2020-07-31 15:56:27 -0300

    inital commit

---
+++ README.md
@@ -0,0 +1,21 @@
+dynamikaweb/yii2-grid 
+====================================
+[![Latest Stable Version](https://img.shields.io/github/v/release/dynamikaweb/yii2-grid)](https://packagist.org/packages/dynamikaweb/yii2-grid) [![Total Downloads](https://poser.pugx.org/dynamikaweb/yii2-grid/downloads)](https://packagist.org/packages/dynamikaweb/yii2-grid) [![License](https://poser.pugx.org/dynamikaweb/yii2-grid/license)](https://github.com/dynamikaweb/yii2-grid/blob/master/LICENSE)[![Codacy Badge](https://app.codacy.com/project/badge/Grade/4c3e83068e31436f916c7bb3e5a8ff57)](https://www.codacy.com/gh/dynamikaweb/yii2-grid?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=dynamikaweb/yii2-grid&amp;utm_campaign=Badge_Grade) [![Build Test](https://scrutinizer-ci.com/g/dynamikaweb/yii2-grid/badges/build.png?b=master)](https://scrutinizer-ci.com/g/dynamikaweb/yii2-grid/) [![Latest Unstable Version](https://poser.pugx.org/dynamikaweb/yii2-grid/v/unstable)](https://packagist.org/packages/dynamikaweb/yii2-grid)
+
+Installation
+------------
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```SHELL
+$ composer require dynamikaweb/yii2-grid "*"
+```
+
+or add
+
+```JSON
+"dynamikaweb/yii2-grid": "*"
+```
+
+to the `require` section of your `composer.json` file.
---
+++ src/grid/ActionColumn.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace dynamikaweb\grid;
+
+use kartik\icons\Icon;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\helpers\ArrayHelper;
+
+
+use Yii;
+
+class ActionColumn extends \yii\grid\ActionColumn
+{
+    public $headerOptions = ['class' => 'action-column', 'width' => '110px'];
+
+    public $redirect = null;
+
+    public $baseUrl = "";
+
+    protected function initDefaultButtons()
+    {
+        $this->initDefaultButton('go', 'external-link-alt', ['target' => '_blank']);
+        $this->initDefaultButton('view', 'eye');
+        $this->initDefaultButton('update', 'edit');
+        $this->initDefaultButton('delete', 'trash', [
+            'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
+            'data-method' => 'post',
+        ]);
+    }
+    
+    protected function initDefaultButton($name, $iconName, $additionalOptions = [])
+    {
+        if (!isset($this->buttons[$name]) && strpos($this->template, '{' . $name . '}') !== false) {
+            $this->buttons[$name] = function ($url, $model, $key) use ($name, $iconName, $additionalOptions) {
+                switch ($name) {
+                    case 'go': 
+                        $title = Yii::t('yii', 'Redirect');
+                        
+                        if ($this->redirect) {
+                            $url = Url::to(["{$this->redirect}", 'id' => $model->id, 'slug' => $model->getAttribute('slug')], true);
+                        } else {
+                            $url = Url::to(["{$this->redirect}/view", 'id' => $model->id, 'slug' => $model->getAttribute('slug')], true);
+                        }
+
+                        $url = str_replace(Yii::$app->request->baseUrl, $this->baseUrl, $url);
+                        break;
+                    case 'view':
+                        $title = Yii::t('yii', 'View');
+                        break;
+                    case 'update':
+                        $title = Yii::t('yii', 'Update');
+                        break;
+                    case 'delete':
+                        $title = Yii::t('yii', 'Delete');
+                        break;
+                    default:
+                        $title = ucfirst($name);
+                }
+                $options = array_merge([
+                    'title' => $title,
+                    'aria-label' => $title,
+                    'data-pjax' => '0',
+                ], $additionalOptions, $this->buttonOptions);
+                $icon = Icon::show($iconName, ['framework' => Icon::FAS]);
+                return Html::a($icon, $url, $options);
+            };
+        }
+    }
+
+}
